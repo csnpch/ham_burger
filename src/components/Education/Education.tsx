3240ff8d4a27824fa513236fedad9305d0b6c4f7
ChitsanuphongChaihong@outlook.com <ChitsanuphongChaihong@outlook.com>
2023-06-19 14:57:42 +0700

    Make to done section education

--- src/components/Education/Education.tsx
+++ src/components/Education/Education.tsx
@@ -1,4 +1,9 @@
 import './Education.sass'
+import EduBanner from '@/assets/img/education_banner.svg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGraduationCap } from '@fortawesome/free-solid-svg-icons'
+import { dataEducation } from './data'
+import { _class } from '@/utils/functions/helper'
 
 
 
@@ -10,7 +15,61 @@ export default function Education() {
 
       <section className={`education-container`}>
         
-        
+          {/* left or top content */}
+          <div className={`banner-img`}>
+            <img src={EduBanner} alt="#" width={'100%'} height={'100%'} />
+          </div>
+
+          {/* right or bottom content */}
+          <div className={`content-container`}>
+            
+            <div className={`topic-container`}>
+              <div className={`title`}>
+                <FontAwesomeIcon icon={faGraduationCap} />
+                <p>MY EDUCATION</p>
+              </div>
+              <p className={`sub-title`}>
+                Education Is Not The Learning Of Facts, But The Training Of The Mind To Think. 
+              </p>
+            </div>
+
+            <div className={`edu-card-container`}>
+              {
+                dataEducation.map((item, index) => (
+                  <div 
+                    key={index}
+                    className={`edu-card-item`}
+                  >
+                    {/* left */}
+                    <div className={`timeline-group`}>
+                      <p className={_class(`
+                        timeline
+                        ${index === 0 && 'color-secondary'}
+                      `)}>
+                        { item.timeline }
+                      </p>
+                      <p className={`status`}>
+                        { item.status }
+                      </p>
+                    </div>
+                    {/* right */}
+                    <div className={`detail-group`}>
+                      <p className={_class(`
+                        title
+                        ${index === 0 && 'color-secondary'}
+                      `)}>
+                        { item.title }
+                      </p>
+                      <p className={`sub-title`}>
+                        { item.subtitle }
+                      </p>
+                    </div>
+                  </div>
+                ))
+              }
+            </div>
+
+          </div>
 
       </section>
 
---
+++ src/components/Education/data.ts
@@ -0,0 +1,28 @@
+interface TimelineEducationInterface {
+  timeline: string
+  title: string
+  subtitle: string
+  status: string
+}
+
+
+export const dataEducation: TimelineEducationInterface[] = [
+  {
+    timeline: `2020`,
+    title: `KMUTNB, Prachinburi Campus`,
+    subtitle: `Bachelor's degree, Infomation technology`,
+    status: `( Studying )`
+  },
+  {
+    timeline: `2017 - 2019`,
+    title: `Nakhon Ratchasima Technical College`,
+    subtitle: `Vocational Certificate, Computer business`,
+    status: `( Completed )`
+  },
+  {
+    timeline: `2014 - 2016`,
+    title: `Ubolratana rajakanya ratchawittayalai Nakhonratchasima school`,
+    subtitle: `Junior high school, General course`,
+    status: `( Completed )`
+  },
+]
